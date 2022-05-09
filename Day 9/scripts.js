d7bfb6cbe697110794b7d52a08ac57d005457ca8
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2022-05-09 22:21:25 +0100

    readme for solutions

--- Day 9/scripts.js
+++ Day 9/scripts.js
@@ -1,53 +1,12 @@
-// function printPascal(n)
-//     {
-         
-//     // Iterate through every line
-//     // and print entries in it
-//     for (let line = 0; line < n; line++)
-//     {
-//         // Every line has number of
-//         // integers equal to line number
-//         for (let i = 0; i <= line; i++)
-//         document.write(binomialCoeff
-//                         (line, i)+" ");
-                         
-//         document.write("<br />");
-//     }
-//     }
-     
-//     // Link for details of this function
-//     // https://www.geeksforgeeks.org/space-and-time-efficient-binomial-coefficient/
-//     function binomialCoeff(n, k)
-//     {
-//         let res = 1;
-         
-//         if (k > n - k)
-//         k = n - k;
-             
-//         for (let i = 0; i < k; ++i)
-//         {
-//             res *= (n - i);
-//             res /= (i + 1);
-//         }
-//         return res;
-//     }
- 
- 
-// // Driver Code
- 
-//     let n = 7;
-//     printPascal(n);
-
-
 function GeneratedTriangle1(rows) {
     var pascalsTriangle = [];
 
-    for (let r = 0; r < rows; r++) {
-        let elements = r + 1;
+    for (let j = 0; j < rows; j++) {
+        let elements = j + 1;
         let level = [];
 
         for (var i = 0; i < elements; i++) {
-            if (i === 0 || i === elements - 1 || r === 1)
+            if (i === 0 || i === elements - 1 || j === 1)
                 level[i] = 1;
             else {
                 let prevRow = pascalsTriangle[r - 1];
@@ -55,7 +14,7 @@ function GeneratedTriangle1(rows) {
             }
         }
 
-        pascalsTriangle[r] = level;
+        pascalsTriangle[j] = level;
     }
 
     return pascalsTriangle;
--- Readme.md
+++ Readme.md
@@ -8,3 +8,7 @@ Day Five solution:  https://replit.com/@Cheerio-ban/30daysOfCodeEcx#Day%205/inde
 Day Six solution: https://replit.com/@Cheerio-ban/30daysOfCodeEcx-1#Day%206/index.js
 Day 7 solution: https://replit.com/@Cheerio-ban/30daysOfCodeEcx-1#Day%207/sample.js
 Day 8 solution: https://replit.com/@Cheerio-ban/30daysOfCodeEcx-3#Day%208/Day8.js
+Day 9 solution: https://replit.com/@Cheerio-ban/30daysOfCodeEcx-5#Day%209/scripts.js
+Day 10 solution: https://replit.com/@Cheerio-ban/30daysOfCodeEcx-5#Day10/script.js
+Day 15 solution: https://cheerio-ban.github.io/order-mehn/
+Day 16 solution: https://cheerio-ban.github.io/order-mehn/
